c23c9fcc3c657690f0a2f36fafca019cf1b9213f
MHDSAEEDRABID <saeedrubid@gmail.com>
2022-02-18 14:43:47 +0200

    Add project files.

---
+++ AngularTechTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AngularTechTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
